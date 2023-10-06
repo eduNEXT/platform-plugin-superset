8e1d585887695e6c6d720f1e015e6ac8b0df65d3
Cristhian Garcia <crisgarta8@gmail.com>
2023-10-06 11:12:31 -0500

    feat: use guest tokens to interact with superset

--- platform_plugin_superset/extensions/filters.py
+++ platform_plugin_superset/extensions/filters.py
@@ -6,50 +6,75 @@ from django.conf import settings
 from django.template import Context, Template
 from openedx_filters import PipelineStep
 from web_fragments.fragment import Fragment
+import os
+from crum import get_current_user
+
+from supersetapiclient.client import SupersetClient
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 TEMPLATE_ABSOLUTE_PATH = "/instructor_dashboard/"
 BLOCK_CATEGORY = "superset"
 
-ASSET_CONFIG = {
-    "title": getattr(settings, "SUPERSET_TITLE", "Superset"),
-    "asset_type": getattr(settings, "SUPERSET_ASSET_TYPE", "slice"),
-    "asset_id": getattr(settings, "SUPERSET_ASSET_ID", "mjRp3DX3EYQ"),
-    "extra_url_params": getattr(settings, "SUPERSET_EXTRA_URL_PARAMS", {}),
-    "superset_url": getattr(settings, "SUPERSET_URL", ""),
-}
 
-def process_config(config):
-    if config.get("asset_type") == "dashboard":
-        config["iframe_url"] = "{superset_url}/superset/dashboard/{asset_id}?standalone=3&show_filters=0&expand_filters=0".format(
-            **config
-        )
-    if config.get("asset_type") == "slice":
-        config["iframe_url"] = "{superset_url}/superset/explore/p/{asset_id}?standalone=1".format(
-            **config
-        )
+def load_guest_token(user, course):
+    superset_host = "http://superset:8088/"
+    superset_username = "9T2Oz1iw0455"
+    superset_password = "SMVWlysdQH1l7NFQMKouJ9NO"
+    dashboard_id = "776a7457-ffb1-4aa1-802e-17e8a00fbcb9"
 
-    for extra_url_param in config["extra_url_params"]:
-        config["iframe_url"] += "&{}={}".format(
-            extra_url_param, config["extra_url_params"][extra_url_param]
-        )
+    client = SupersetClient(
+        host=superset_host,
+        username=superset_username,
+        password=superset_password,
+    )
 
-    return config
+    course_run = course.children[0].course_key.run
 
+    data = {
+        "user": {
+            "username": user.username,
+            "first_name": "John",
+            "last_name": "Doe",
+        },
+        "resources": [
+            {"type": "dashboard", "id": dashboard_id}
+        ],
+        "rls": [
+            {"clause": f"org = '{(course.org)}'"},
+            {"clause": f"course_name = '{(course.display_name)}'"},
+            {"clause": f"course_run = '{(course_run)}'"},
+            ]
+    }
+
+    response = client.session.post(
+        url=superset_host + "api/v1/security/guest_token/",
+        json=data,
+        headers={"Content-Type": "application/json"},
+    )
+
+    return response.json()["token"], dashboard_id
 
 
 class AddSupersetTab(PipelineStep):
     """Add superset tab to instructor dashboard."""
 
-    def run_filter(self, context, template_name):  # pylint: disable=arguments-differ, unused-argument
+    def run_filter(
+        self, context, template_name
+    ):  # pylint: disable=arguments-differ, unused-argument
         """Execute filter that modifies the instructor dashboard context.
         Args:
             context (dict): the context for the instructor dashboard.
             _ (str): instructor dashboard template name.
         """
         course = context["course"]
+
+        user = get_current_user()
+        superset_token, dashboard_id = load_guest_token(user, course)
         context.update(
             {
-                "superset_config": process_config(ASSET_CONFIG),
+                "superset_token": superset_token,
+                "dashboard_id": dashboard_id,
             }
         )
         template = Template(self.resource_string("static/html/superset.html"))
